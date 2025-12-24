9de3035bda422d9caa5714edf4fd7898ac3af902
Wataru Nishimoto <nishimoto.w@note.jp>
2025-12-24 13:06:55 +0900

    chore:add Ko Reader for Kindle

---
+++ lib/device/kokindle.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+#
+# Copyright 2013 whiteleaf. All rights reserved.
+#
+
+require "fileutils"
+
+module Device::Kokindle
+  PHYSICAL_SUPPORT = true
+  VOLUME_NAME = "Kindle"
+  DOCUMENTS_PATH_LIST = %w(documents Documents Books)
+  EBOOK_FILE_EXT = ".epub"
+  NAME = "KoKindle"
+  DISPLAY_NAME = "Kindle"
+
+  RELATED_VARIABLES = {
+    "default.enable_half_indent_bracket" => true,
+  }
+
+  include Device::BackupBookmarkUtility
+
+  def backup_bookmark
+    documents_path = get_documents_path
+    raise Device::DontConneting unless documents_path
+    paths = Dir.glob(File.join(documents_path, "*.sdr/*.azw3{f,r}"))
+    Helper.copy_files(paths, get_storage_path, exception: false)
+    paths.size
+  end
+
+  def restore_bookmark
+    documents_path = get_documents_path
+    raise Device::DontConneting unless documents_path
+    paths = Dir.glob(File.join(get_storage_path, "*.sdr/*"))
+    Helper.copy_files(paths, documents_path, exception: false)
+    paths.size
+  end
+end
